8f5eb50588438a289010227793711cf4cd1a5762
Omor Faruqe <omor.software.dev@gmail.com>
2026-07-25 15:43:18 +0600

    Sprint 04.4 Route Guard completed

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -6,32 +6,46 @@ import '../features/dashboard/presentation/pages/dashboard_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
-  redirect: (context, state) {
-    final loggedIn = SupabaseService.client.auth.currentUser != null;
 
-    if (loggedIn) {
+  redirect: (context, state) {
+    final loggedIn =
+        SupabaseService.client.auth.currentUser != null;
+
+    final isLoginRoute = state.matchedLocation == '/login';
+
+    // User is NOT logged in
+    if (!loggedIn) {
+      return isLoginRoute ? null : '/login';
+    }
+
+    // User IS logged in
+    if (loggedIn && isLoginRoute) {
       return '/dashboard';
     }
 
-    return '/login';
+    // Allow access
+    return null;
   },
+
   routes: [
     GoRoute(
       path: '/',
-      redirect: (_, __) {
+      redirect: (context, state) {
         final loggedIn =
             SupabaseService.client.auth.currentUser != null;
 
         return loggedIn ? '/dashboard' : '/login';
       },
     ),
+
     GoRoute(
       path: '/login',
-      builder: (_, __) => const LoginPage(),
+      builder: (context, state) => const LoginPage(),
     ),
+
     GoRoute(
       path: '/dashboard',
-      builder: (_, __) => const DashboardPage(),
+      builder: (context, state) => const DashboardPage(),
     ),
   ],
 );
